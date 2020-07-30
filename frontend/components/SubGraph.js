2ca5a748daf395bd0a466f03549025c96eb88bbd
jeonghye-choi <ppllhm.0@gmail.com>
2020-07-30 17:43:18 +0900

    front: Initial setting & layout
    기본 세팅
    main page, dashboard page 레이아웃

---
+++ frontend/components/SubGraph.js
@@ -0,0 +1,16 @@
+import React from 'react';
+// import Link from 'next/link';
+
+const SubGraph = ({no}) => {
+  return(
+    <>
+      <div className="subGraph">
+          서브그래프 {no}
+          <br/><br/><br/><br/><br/>dfd<br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
+
+      </div>
+    </>
+  );
+};
+
+export default SubGraph;
