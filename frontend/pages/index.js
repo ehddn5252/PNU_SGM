ca3c269e938adada77b05a25b0760c1310662b7b
jeonghye-choi <ppllhm.0@gmail.com>
2020-08-04 18:11:47 +0900

    Add style to index page
    인덱스 페이지 scss 스타일 적용
    기본 레이아웃 완성

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -6,17 +6,22 @@ const Home = () => {
   return (
     <div className="index">
       <nav>
-        <span className="logo">Logo</span>
-        <span className="linkBtn">linkBtn</span>
-        <span className="linkBtn">linkBtn</span>
-        <span className="linkBtn">linkBtn</span>
-        <span className="profile">profile</span>
+        <span className="logo"><Link href="/"><a>Logo</a></Link></span>
+        <span className="linkBtn-container">
+          <li className="linkBtn"><Link href=""><a>linkBtn</a></Link></li>
+          <li className="linkBtn"><Link href=""><a>linkBtn</a></Link></li>
+          <li className="linkBtn"><Link href=""><a>linkBtn</a></Link></li>
+        </span>
+        
+        <span className="profile"><Link href=""><a>profile</a></Link></span>
       </nav>
-      <section>
+      <section className="main">
+        {/* main */}
         <article>
           <div>Welcome!</div>
           <div>
             Maxturn - 수익률을 확인하고 투자하세요.
+            <br></br>
             맥스턴은 백테스트를 진행하고, 이에 대한 전략을 수립하고 유저들과 공유하는 플랫폼입니다.
           </div>
           <div>
@@ -24,30 +29,49 @@ const Home = () => {
           </div>
         </article>
       </section>
-      <section>
+
+      <section className="introduction">
+        {/* introduction */}
+        <div className="introduction_title">INTRODUCTION</div>
         <article>
-          <div>photo</div>
-          <div>설명</div>
+          <div>
+            <figure>photo</figure>
+            <figcaption>설명</figcaption>
+          </div>
+        </article>
+
+        {/* <Pagination/> */}
+
+        {/* <article>
+          <figure>photo</figure>
+          <figcaption>설명</figcaption>
         </article>
         <article>
-          <div>photo</div>
-          <div>설명</div>
-        </article>
-        <article>
-          <div>photo</div>
-          <div>설명</div>
-        </article>
+          <figure>photo</figure>
+          <figcaption>설명</figcaption>
+        </article> */}
       </section>
-      <section>
+
+      <section className="login">
+        {/* 로그인 */}
         <article>
           <form>
             <div>Log in to your account</div>
-            <input type="text"></input>
-            <input type="password"></input>
+            <div className="idBox">
+              <label for="user_id">Username</label>
+              <input type="text" id="user_id"></input>
+            </div>
+            <div className="pwBox">
+              <label for="user_password">Password</label>
+              <input type="password"></input>
+            </div>
             <button type="submit"></button>
           </form>
         </article>
       </section>
+      <footer>
+
+      </footer>
     </div>
   );
 };
