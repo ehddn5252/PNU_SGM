22f62bc673f9ba1fc686bc072a64b3f52031dd18
jeonghye-choi <ppllhm.0@gmail.com>
2020-08-31 22:11:03 +0900

    Call data from API to backtest page; Items components
    api에 더미 데이터로 넣어둔 종목리스트 불러옴
    
    - 임의로 버튼을 만들어둠
    - 나중에 백테스팅 버튼 onclick됐을 때랑 연결해야함!

--- frontend/actions/winRateActions.js
+++ frontend/actions/winRateActions.js
@@ -1,28 +1,26 @@
 export const winRate_getData = () => async dispatch => {
-    try {
-      dispatch({
-        type: "AWAITING_WINRATE"
-      })
-  
-      const res = await fetch('http://localhost:3000/api/winRatetest');
-      const doc = await res.json();
-      
-      const winRate_data = [];
+  try {
+    dispatch({
+      type: "AWAITING_WINRATE"
+    })
 
-      winRate_data.push(doc.win);
-      winRate_data.push(doc.lose);
-  
-  
-      dispatch({
-        type: "SUCCESS_WINRATE",
-        payload: {
-          winRate_data
-        }
-      })
-    } catch (e) {
-      dispatch({
-        type: "REJECTED_WINRATE",
-      })
-    }
+    const res = await fetch("http://localhost:3000/api/winRatetest")
+    const doc = await res.json()
+
+    const winRate_data = []
+
+    winRate_data.push(doc.win)
+    winRate_data.push(doc.lose)
+
+    dispatch({
+      type: "SUCCESS_WINRATE",
+      payload: {
+        winRate_data
+      }
+    })
+  } catch (e) {
+    dispatch({
+      type: "REJECTED_WINRATE"
+    })
   }
-  
+}
